5e251800087df7c590d89294344674b739a311bd
Ruslan.Sokolov <vaquero1master@gmail.com>
2025-10-02 13:52:02 +0200

    fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
+import Webcam from "react-webcam";
+
 import styles from "./App.module.css"
 
 function App() {
+  const [showWebcam, setShowWebkam] = useState(false);
+  // const WebcamComponent = () => <Webcam />;
+  const cameraTurnOn = () => {
+    setShowWebkam(true);
+  }
 
   return (
     <>
       <div className={styles.wrapGreeting}>
         <p className={styles.greeting}>Hello App!!!</p>
       </div>
+      <div className={styles.wrapBtn}>
+        <button onClick={cameraTurnOn} className={styles.btn}>Scan pH strip </button>
+      </div>
+      {showWebcam && (
+        <div>
+          <Webcam />
+        </div>
+      )}
+
+
 
 
 
