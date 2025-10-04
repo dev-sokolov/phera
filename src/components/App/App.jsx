f404a4115ac6f34eea49d9fcddb5f2e78122f37d
Ruslan.Sokolov <vaquero1master@gmail.com>
2025-10-04 15:02:10 +0200

    adding camera handlers

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,27 +1,88 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Webcam from "react-webcam";
 
 import styles from "./App.module.css"
 
 function App() {
-  const [showWebcam, setShowWebkam] = useState(false);
+  const [isCameraOn, setIsCameraOn] = useState(false);
+  const [capturedImage, setCapturedImage] = useState(null);
+
+  const webcamRef = useRef(null);
   // const WebcamComponent = () => <Webcam />;
-  const cameraTurnOn = () => {
-    setShowWebkam(true);
+  const handleStartCamera = () => {
+    setIsCameraOn(true);
+    setCapturedImage(null); // сброс предыдущего фото
   }
+
+  const handleStopCamera = () => {
+    if (webcamRef.current) {
+      const tracks = webcamRef.current.video.srcObject?.getTracks();
+      tracks?.forEach((track) => track.stop());
+    }
+    setIsCameraOn(false);
+  };
+
+  // Настройки камеры: задняя камера на смартфонах
+  // const videoConstraints = {
+  //   facingMode: { exact: "environment" }, // или просто "environment"
+  // };
+
+  const handleCapture = () => {
+    if (webcamRef.current) {
+      const imageSrc = webcamRef.current.getScreenshot();
+      setCapturedImage(imageSrc);
+      handleStopCamera(); // остановка камеры после снимка (по желанию)
+    }
+  };
 
   return (
     <>
       <div className={styles.wrapGreeting}>
         <p className={styles.greeting}>Hello App!!!</p>
       </div>
+
       <div className={styles.wrapBtn}>
-        <button onClick={cameraTurnOn} className={styles.btn}>Scan pH strip </button>
+        {!isCameraOn && !capturedImage && (
+          // {!isCameraOn && (
+          <button onClick={handleStartCamera} className={styles.btn}>
+            Start Camera
+          </button>
+        )}
+        {isCameraOn && (
+          <>
+            <button onClick={handleCapture} className={styles.btn}>
+              Scan pH strip
+            </button>
+            <button onClick={handleStopCamera} className={styles.btn}>
+              Stop Camera
+            </button>
+          </>
+        )}
       </div>
-      {showWebcam && (
+
+      {isCameraOn && (
+        <div className={styles.webcamWrap}>
+          <Webcam ref={webcamRef} audio={false} screenshotFormat="image/png" />
+        </div>
+      )}
+      {/* {showWebcam && (
         <div>
           <Webcam />
         </div>
+      )} */}
+      {/* ----------------------- */}
+
+      {capturedImage && (
+        <>
+          <div className={styles.capturedWrap}>
+            <p>Captured Image:</p>
+            <img src={capturedImage} alt="pH strip" className={styles.capturedImg} />
+            <button onClick={handleStartCamera} className={styles.btn}>
+              Retake
+            </button>
+          </div>
+        </>
+
       )}
 
 
