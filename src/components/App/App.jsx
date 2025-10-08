a19979cf1ddcce6a863f34db198d89db59effdd7
Ruslan.Sokolov <vaquero1master@gmail.com>
2025-10-08 12:32:03 +0200

    fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,129 +1,17 @@
-import { useState, useRef } from "react";
-import Webcam from "react-webcam";
+import HomePage from "../../pages/HomePage/HomePage";
 
-import styles from "./App.module.css"
+import "../../shared/styles/style.css";
 
 function App() {
-  const [isCameraOn, setIsCameraOn] = useState(false);
-  const [capturedImage, setCapturedImage] = useState(null);
-
-  const webcamRef = useRef(null);
-  // const WebcamComponent = () => <Webcam />;
-
-
-  // Настройки камеры: задняя камера на смартфонах
-  // const videoConstraints = {
-  //   facingMode: { ideal: "environment" }, // или просто "environment"
-  // };
-
-  const videoConstraints = {
-    width: { ideal: 1280 },
-    height: { ideal: 720 },
-    facingMode: { ideal: "environment" },
-  };
-
-  // const handleStartCamera = () => {
-  //   setIsCameraOn(true);
-  //   setCapturedImage(null); // сброс предыдущего фото
-  // }
-
-  const handleStartCamera = async () => {
-    try {
-      setIsCameraOn(true);
-      setCapturedImage(null);
-    } catch (err) {
-      console.error("Error starting camera:", err);
-    }
-  };
-
-  const handleStopCamera = () => {
-    if (webcamRef.current) {
-      const tracks = webcamRef.current.video.srcObject?.getTracks();
-      tracks?.forEach((track) => track.stop());
-    }
-    setIsCameraOn(false);
-  };
-
-  const handleCapture = () => {
-    if (webcamRef.current) {
-      const imageSrc = webcamRef.current.getScreenshot();
-      setCapturedImage(imageSrc);
-      handleStopCamera(); // остановка камеры после снимка (по желанию)
-    }
-  };
-
-  // const handleReset = () => {
-  //   setCapturedImage(null); // сброс предыдущего фото
-  // }
-
-  const handleReset = () => {
-    if (webcamRef.current) {
-      const tracks = webcamRef.current.video.srcObject?.getTracks();
-      tracks?.forEach((track) => track.stop());
-    }
-    setCapturedImage(null);
-    setIsCameraOn(false);
-  };
 
   return (
     <>
-      <div className={styles.wrapGreeting}>
-        <p className={styles.greeting}>Welcome page</p>
-      </div>
-
-      <div className={styles.wrapBtn}>
-        {!isCameraOn && !capturedImage && (
-          // {!isCameraOn && (
-          <button onClick={handleStartCamera} className={styles.btn}>
-            Start Camera
-          </button>
-        )}
-        {isCameraOn && (
-          <>
-            <button onClick={handleCapture} className={styles.btn}>
-              Scan pH strip
-            </button>
-            <button onClick={handleStopCamera} className={styles.btn}>
-              Stop Camera
-            </button>
-          </>
-        )}
-      </div>
-
-      {isCameraOn && (
-        <div className={styles.webcamWrap}>
-          <Webcam
-            ref={webcamRef}
-            audio={false}
-            screenshotFormat="image/png"
-            videoConstraints={videoConstraints}
-          />
-        </div>
-      )}
-      {/* ----------------------- */}
-
-      {capturedImage && (
-        <>
-          <div className={styles.capturedWrap}>
-            <p>Captured Image:</p>
-            <img src={capturedImage} alt="pH strip" className={styles.capturedImg} />
-            <button onClick={handleStartCamera} className={styles.btn}>
-              Retake
-            </button>
-            <button onClick={handleReset} className={styles.btn}>
-              Reset
-            </button>
-          </div>
-        </>
-
-      )}
-
+      <HomePage />
     </>
   )
 }
 
-export default App
-
+export default App;
 
 // -----------------------------------------------------
 
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,105 @@
+import { useState, useRef } from "react";
+import Webcam from "react-webcam";
+
+import styles from "./HomePage.module.css";
+
+const HomePage = () => {
+    const [isCameraOn, setIsCameraOn] = useState(false);
+    const [capturedImage, setCapturedImage] = useState(null);
+
+    const webcamRef = useRef(null);
+
+    // Настройки камеры: задняя камера на смартфонах
+
+    const videoConstraints = {
+        facingMode: { ideal: "environment" },
+    };
+
+    const handleStartCamera = async () => {
+        try {
+            setIsCameraOn(true);
+            setCapturedImage(null);  // сброс предыдущего фото
+        } catch (err) {
+            console.error("Error starting camera:", err);
+        }
+    };
+
+    const handleStopCamera = () => {
+        if (webcamRef.current) {
+            const tracks = webcamRef.current.video.srcObject?.getTracks();
+            tracks?.forEach((track) => track.stop());
+        }
+        setIsCameraOn(false);
+    };
+
+    const handleCapture = () => {
+        if (webcamRef.current) {
+            const imageSrc = webcamRef.current.getScreenshot();
+            setCapturedImage(imageSrc);
+            handleStopCamera();
+        }
+    };
+
+    const handleReset = () => {
+        if (webcamRef.current) {
+            const tracks = webcamRef.current.video.srcObject?.getTracks();
+            tracks?.forEach((track) => track.stop());
+        }
+        setCapturedImage(null);
+        setIsCameraOn(false);
+    };
+
+    return (
+        <>
+            <div className={styles.wrapGreeting}>
+                <p className={styles.greeting}>Welcome page</p>
+            </div>
+
+            <div className={styles.wrapBtn}>
+                {!isCameraOn && !capturedImage && (
+                    <button onClick={handleStartCamera} className={styles.btn}>
+                        Turn on the camera
+                    </button>
+                )}
+                {isCameraOn && (
+                    <>
+                        <button onClick={handleCapture} className={styles.btn}>
+                            Scan pH strip
+                        </button>
+                        <button onClick={handleStopCamera} className={styles.btn}>
+                            Home
+                        </button>
+                    </>
+                )}
+            </div>
+
+            {isCameraOn && (
+                <div className={styles.webcamWrap}>
+                    <Webcam
+                        ref={webcamRef}
+                        audio={false}
+                        screenshotFormat="image/png"
+                        videoConstraints={videoConstraints}
+                    />
+                </div>
+            )}
+
+            {capturedImage && (
+                <>
+                    <div className={styles.capturedWrap}>
+                        <p>Captured Image:</p>
+                        <img src={capturedImage} alt="pH strip" className={styles.capturedImg} />
+                        <button onClick={handleStartCamera} className={styles.btn}>
+                            Retake
+                        </button>
+                        <button onClick={handleReset} className={styles.btn}>
+                            Home
+                        </button>
+                    </div>
+                </>
+            )}
+        </>
+    )
+}
+
+export default HomePage;
